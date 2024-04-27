26dcdf9b5b1cd74383222fc2b015987b30187ea5
Ivan Lyutko <ivan.lyutko@bk.ru>
2024-04-27 13:42:10 +0700

    Добавлен класс unit

---
+++ Unit.h
@@ -0,0 +1,22 @@
+#ifndef UNIT_H
+#define UNIT_H
+#include <iostream>
+
+using std::string;
+using std::runtime_error;
+using std::shared_ptr;
+
+
+class Unit
+{
+public:
+    using Flags = unsigned int;
+    virtual ~Unit() = default;
+    virtual void add(const shared_ptr<Unit>&, Flags);
+    virtual string compile(unsigned int level = 0) const = 0;
+
+protected:
+    virtual string generateShift(unsigned int level) const;
+};
+
+#endif // UNIT_H
